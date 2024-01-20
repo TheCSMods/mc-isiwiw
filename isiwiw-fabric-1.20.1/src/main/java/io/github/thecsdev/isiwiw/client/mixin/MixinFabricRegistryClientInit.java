4c0913da33611e807d8450d9fb842ef54a0f64fe
Dave <thecsdev@gmail.com>
2024-01-20 19:12:49 +0100

    Release v1.1+fabric
    Partial Quilt support.
    - Fabric clients can now use the bypass on Quilt servers.
    - Quilt clients however, were not tested with this mod's version, so I don't yet know if this works on Quilt clients.

--- isiwiw-fabric-1.20.1/src/main/java/io/github/thecsdev/isiwiw/client/mixin/MixinFabricRegistryClientInit.java
+++ isiwiw-fabric-1.20.1/src/main/java/io/github/thecsdev/isiwiw/client/mixin/MixinFabricRegistryClientInit.java
@@ -1,23 +1,52 @@
 package io.github.thecsdev.isiwiw.client.mixin;
 
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.github.thecsdev.isiwiw.ISIWIW;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking.PlayChannelHandler;
 import net.fabricmc.fabric.impl.client.registry.sync.FabricRegistryClientInit;
+import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.fabricmc.fabric.impl.registry.sync.packet.RegistryPacketHandler;
+import net.minecraft.text.Text;
 
 /**
  * Client-sided Mixin.
  * Prevents 'Fabric API' from forcing users to install mods present on the server.
  */
-@Mixin(value = FabricRegistryClientInit.class, remap = false)
+@Mixin(value = FabricRegistryClientInit.class, remap = false, priority = Integer.MAX_VALUE)
 public abstract class MixinFabricRegistryClientInit
 {
-	@Inject(method = "onInitializeClient", at = @At("HEAD"), cancellable = true, require = 0)
-	private void onOnInitializeClient(CallbackInfo ci)
+	// ==================================================
+	private static @Shadow Logger LOGGER;
+	protected abstract @Shadow Text getText(Exception e);
+	// ==================================================
+	@Inject(method = "registerSyncPacketReceiver", at = @At("HEAD"), cancellable = true, require = 0)
+	private void onRegisterSyncPacketReceiver(RegistryPacketHandler packetHandler, CallbackInfo ci)
 	{
-		//stop it Fabric, get some help...
+		//create a play channel handler
+		final PlayChannelHandler pch = (client, handler, buf, responseSender) ->
+		{
+			RegistrySyncManager.receivePacket(
+					client, packetHandler, buf,
+					RegistrySyncManager.DEBUG || !client.isInSingleplayer(), e ->
+			{
+				LOGGER.error("Registry remapping failed!", e);
+				ISIWIW.LOGGER.warn("Preventing 'Fabric API' from kicking the client...");
+				//client.execute(() -> handler.getConnection().disconnect(getText(e))); -- THIS IS TO BE REMOVED!
+			});
+		};
+		
+		//register the play channel handler
+		ClientPlayNetworking.registerGlobalReceiver(packetHandler.getPacketId(), pch);
+		
+		//DO NOT use Fabric's mechanism.
 		ci.cancel();
 	}
+	// ==================================================
 }
--- isiwiw-fabric-1.20.2/src/main/java/io/github/thecsdev/isiwiw/client/mixin/MixinFabricRegistryClientInit.java
+++ isiwiw-fabric-1.20.2/src/main/java/io/github/thecsdev/isiwiw/client/mixin/MixinFabricRegistryClientInit.java
@@ -1,23 +1,61 @@
 package io.github.thecsdev.isiwiw.client.mixin;
 
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.github.thecsdev.isiwiw.ISIWIW;
+import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.impl.client.registry.sync.FabricRegistryClientInit;
+import net.fabricmc.fabric.impl.registry.sync.FabricRegistryInit;
+import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.fabricmc.fabric.impl.registry.sync.packet.RegistryPacketHandler;
+import net.minecraft.text.Text;
 
 /**
  * Client-sided Mixin.
  * Prevents 'Fabric API' from forcing users to install mods present on the server.
  */
-@Mixin(value = FabricRegistryClientInit.class, remap = false)
+@Mixin(value = FabricRegistryClientInit.class, remap = false, priority = Integer.MAX_VALUE)
 public abstract class MixinFabricRegistryClientInit
 {
-	@Inject(method = "onInitializeClient", at = @At("HEAD"), cancellable = true, require = 0)
-	private void onOnInitializeClient(CallbackInfo ci)
+	// ==================================================
+	private static @Shadow Logger LOGGER;
+	protected abstract @Shadow Text getText(Throwable e);
+	// ==================================================
+	@Inject(method = "registerSyncPacketReceiver", at = @At("HEAD"), cancellable = true, require = 0)
+	private void onRegisterSyncPacketReceiver(RegistryPacketHandler packetHandler, CallbackInfo ci)
 	{
-		//stop it Fabric, get some help...
+		//register the configuration channel handler
+		ClientConfigurationNetworking.registerGlobalReceiver(packetHandler.getPacketId(), (client, handler, buf, responseSender) ->
+		{
+			RegistrySyncManager.receivePacket(client, packetHandler, buf, RegistrySyncManager.DEBUG || !client.isInSingleplayer())
+				.whenComplete((complete, throwable) ->
+				{
+					//if there's a throwable, then the synch failed. just log it
+					if (throwable != null)
+					{
+						//log the registry synchronization failure
+						LOGGER.error("Registry remapping failed!", throwable);
+						ISIWIW.LOGGER.warn("Preventing 'Fabric API' from kicking the client...");
+						
+						//do not kick!
+						/*client.execute(() -> ((ClientCommonNetworkHandlerAccessor) handler)
+								.getConnection().disconnect(getText(throwable))); -- THIS IS TO BE REMOVED!*/
+					}
+					
+					//i guess lie to the server about sync completion?
+					handler.sendPacket(ClientConfigurationNetworking.createC2SPacket(
+							FabricRegistryInit.SYNC_COMPLETE_ID, PacketByteBufs.create()));
+				});
+		});
+
+		//DO NOT use Fabric's mechanism.
 		ci.cancel();
 	}
+	// ==================================================
 }
